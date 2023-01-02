09be5b1ae8fc103bffabe619ede7bdda329a9760
jmatthewa <jamesm.atanacio@gmail.com>
2023-01-01 22:27:27 -0800

    Level 2 - Encryption

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
-
+const encrypt = require('mongoose-encryption');
 
 const app = express();
 
@@ -20,6 +20,13 @@ mongoose.connect('mongodb://localhost:27017/userDB', {
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
+});
+
+const secret = 'Thisisourlittlesecret.'
+
+userSchema.plugin(encrypt, {
+  secret: secret,
+  encryptedFields: ['password']
 });
 
 

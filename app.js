788bbe73777b5210bc0b7437e6972ce09ffc57b1
jmatthewa <jamesm.atanacio@gmail.com>
2023-01-03 08:24:16 -0800

    Google OAuth 2.0 Authentication

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const ejs = require('ejs');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 
@@ -30,16 +32,45 @@ mongoose.connect('mongodb://localhost:27017/userDB', {
 
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
+  googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model('User', userSchema);
 
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+
+passport.serializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, {
+      id: user.id
+    });
+  });
+});
+
+passport.deserializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: 'http://localhost:3000/auth/google/secrets',
+    // userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get('/', async function(req, res) {
   try {
@@ -48,6 +79,17 @@ app.get('/', async function(req, res) {
     res.send(e);
   }
 });
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] })
+);
+
+app.get('/auth/google/secrets',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
 
 app.get('/login', async function(req, res) {
   try {
@@ -112,14 +154,13 @@ app.post('/login', async function(req, res) {
       password: req.body.password
     });
 
-    req.login(user, function(err) {
+    req.login(user, async function(err) {
       if (err) {
         console.log(err);
       } else {
-        passport.authenticate('local')(req, res, function() {
+        await passport.authenticate('local');
           res.redirect('/secrets');
-        })
-      }
+        }
     });
 
   } catch (e) {
